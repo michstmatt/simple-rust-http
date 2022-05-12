238da4cae1a7486727040780709ad2734e4de4ac
Matt Pasco <michstmatt@gmail.com>
2022-05-12 00:36:35 -0600

    dns working

--- src/dns/mod.rs
+++ src/dns/mod.rs
@@ -8,6 +8,16 @@ pub enum OpCodeEnum{
     IQuery = 1,
     Status = 2,
 }
+impl OpCodeEnum {
+    pub fn from_int(val: u16) ->OpCodeEnum {
+        match val {
+            0 => OpCodeEnum::Query,
+            1 => OpCodeEnum::IQuery,
+            _ => OpCodeEnum::Status 
+        }
+    } 
+}
+
 pub enum RCodeEnum{
     NoErr = 0,
     FormatErr = 1,
@@ -16,7 +26,52 @@ pub enum RCodeEnum{
     NotImplErr = 4,
     RefusedErr = 5
 }
+impl RCodeEnum {
+    pub fn from_int(val: u16) ->RCodeEnum {
+        match val {
+            0 => RCodeEnum::NoErr, 
+            1 => RCodeEnum::FormatErr, 
+            2 => RCodeEnum::ServerErr, 
+            3 => RCodeEnum::NameErr, 
+            4 => RCodeEnum::NotImplErr, 
+            _ => RCodeEnum::RefusedErr
+        }
+    } 
+}
 
+struct Header2 {
+    qr: bool,
+    op_code: OpCodeEnum,
+    aa: bool,
+    tc: bool,
+    rd: bool,
+    ra: bool,
+    r_code: RCodeEnum
+}
+
+impl Header2 {
+    pub fn to_int(self) -> u16{
+        let mut line_2 = (self.qr as u16) << 15;
+        line_2 += (self.op_code as u16) << 11;
+        line_2 += (self.aa as u16) << 10;
+        line_2 += (self.tc as u16) << 9;
+        line_2 += (self.rd as u16) << 8;
+        line_2 += (self.ra as u16) << 7;
+        line_2 += self.r_code as u16;
+        return line_2;
+    }
+    pub fn from_int(data: u16) -> Header2 {
+        return Header2 {
+            qr: (data >> 15) == 1, 
+            op_code: OpCodeEnum::from_int((data & 0x7800)>>11),
+            aa: (data & 0x400) == 0x400,
+            tc: (data & 0x200) == 0x200,
+            rd: (data & 0x100) == 0x100,
+            ra: (data & 0x80) == 0x80,
+            r_code: RCodeEnum::from_int(data & 0xF) 
+        }
+    }
+}
 pub struct DnsHeader {
     id: u16,
     header2: u16,
@@ -27,16 +82,7 @@ pub struct DnsHeader {
 }
 
 impl DnsHeader {
-    pub fn create_header2(qr: bool, op_code: OpCodeEnum, aa: bool, tc: bool, rd:bool, ra:bool, r_code: RCodeEnum) -> u16{
-        let mut line_2 = (qr as u16) << 15;
-        line_2 += (op_code as u16) << 11;
-        line_2 += (aa as u16) << 10;
-        line_2 += (tc as u16) << 9;
-        line_2 += (rd as u16) << 8;
-        line_2 += (ra as u16) << 7;
-        line_2 += r_code as u16;
-        return line_2;
-    }
+
 }
 
 pub struct DnsQuestion {
@@ -63,19 +109,41 @@ impl DnsQuery {
             std::slice::from_raw_parts(&self.question as *const DnsQuestion as *const u8, q_size)
         };
 
-        let size = h_size + q_size + self.name.len();
+        let size = h_size + q_size + self.name.len() + 1;
         let mut packet: Vec<u8> = vec![0u8; size+1];
         packet[0..h_size].copy_from_slice(header_bits);
 
         let end_name = h_size + self.name.len();
-        packet[h_size .. end_name].copy_from_slice(self.name.as_bytes());
-
-        packet[end_name..size].copy_from_slice(question_bits);
+        let name_bytes = self.name.as_bytes();
+        packet[h_size .. end_name].copy_from_slice(name_bytes);
+        packet[end_name] = 0;
+        packet[end_name+1..size].copy_from_slice(question_bits);
 
         return packet;
     }
+
 }
 
+pub struct DnsResponse {
+    header: DnsHeader,
+}
+impl DnsResponse {
+    fn byte_to_u16(bytes: &[u8], index: usize) -> u16 {
+        return (bytes[index+1] as u16) << 8 | bytes[index] as u16;
+    }
+
+    pub fn from_bytes(bytes: &[u8]) -> DnsHeader{
+        let header = DnsHeader {
+            id: DnsResponse::byte_to_u16(&bytes, 0) ,
+            header2: DnsResponse::byte_to_u16(&bytes, 2),
+            qd_count: DnsResponse::byte_to_u16(&bytes, 4),
+            an_count: DnsResponse::byte_to_u16(&bytes, 6),
+            ns_count: DnsResponse::byte_to_u16(&bytes, 8),
+            ar_count: DnsResponse::byte_to_u16(&bytes, 10)
+        };
+        return header;
+    }
+}
 
 pub struct DnsResolver {}
 
@@ -83,10 +151,19 @@ impl DnsResolver {
 
     pub fn get_host_by_name(host: &str)
     {
-        let header_ln2 = DnsHeader::create_header2(false, OpCodeEnum::Query, false, false, true, false, RCodeEnum::NoErr);
+        let header_ln2 = Header2{
+            qr: false,
+            op_code: OpCodeEnum::Query,
+            aa: false,
+            tc: false,
+            rd: true, 
+            ra: false,
+            r_code:RCodeEnum::NoErr
+        };
+
         let dns_header = DnsHeader{
-            id: 0,
-            header2: header_ln2,
+            id: 1337,
+            header2: header_ln2.to_int(),
             qd_count: 1,
             an_count: 0,
             ns_count: 0,
@@ -94,7 +171,7 @@ impl DnsResolver {
         };
 
         let question = DnsQuestion {
-            q_type: 'A' as u16,
+            q_type:  1,
             q_class: 1
         };
 
@@ -102,16 +179,18 @@ impl DnsResolver {
         let query = DnsQuery {
             question: question,
             header: dns_header,
-            name:fmt
+            name: String::from(host)
         };
 
         let packet = query.as_bytes();
 
-        let socket = UdpSocket::bind("127.1.1.1:5353").unwrap();
-        let success = socket.send_to(&packet, "172.18.64.1:53").is_ok();
+        let socket = UdpSocket::bind((Ipv4Addr::UNSPECIFIED, 0)).unwrap();
+        let success = socket.send_to(packet.as_ref(), "8.8.8.8:53").is_ok();
 
         let mut buf = [0; 2048];
-        socket.recv_from(&mut buf).unwrap();
+        let len = socket.recv_from(&mut buf).unwrap();
+        let header = DnsResponse::from_bytes(&buf);
+        let h2 = Header2::from_int(header.header2);
         println!("{:?}", buf);
     }
 
